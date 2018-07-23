c02a5a537dec66a912234a0343a43a0a3f352285
Varun Madiath <vamega@gmail.com>
2018-07-22 23:34:01 -0400

    Format entire repo with black

--- structlog_journald/__init__.py
+++ structlog_journald/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for structlog-journald."""
 
 __author__ = """Varun Madiath"""
-__email__ = 'vamega@gmail.com'
-__version__ = '0.1.0'
+__email__ = "vamega@gmail.com"
+__version__ = "0.1.0"

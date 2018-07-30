b955f02d1afc949e31224eb0f7c5311a4455cf8f
Varun Madiath <vamega@gmail.com>
2018-07-30 01:30:40 -0400

    WIP

--- structlog_journald/structlog_journald.py
+++ structlog_journald/structlog_journald.py
@@ -1,3 +1,42 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import systemd.journal as journal
+
+
+class JournalLoggerFactory(object):
+    """
+    Produce :class:`JournalLogger`\ s.
+
+    To be used with :func:`structlog.configure`\ 's `logger_factory`.
+
+    Positional arguments are silently ignored.
+
+    """
+
+    def __call__(self, *args):
+        return JournalLogger()
+
+
+class JournalLogger(object):
+    """
+    Write events into the systemd journal.
+
+    >>> from structlog import JournalLogger
+    >>> JournalLogger().msg("hello")
+    # In journalctl -> hello
+
+    """
+
+    def __repr__(self):
+        return "<JournalLogger>"
+
+    def msg(self, message, *args, **kwargs):
+        """
+        Log *message* into the journal.
+        """
+        journal.send(message)
+
+    log = debug = info = warn = warning = msg
+    failure = err = error = critical = exception = msg
